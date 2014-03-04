d16ccbc6b3622942547c9d44084144515255b0b4
Bastien Abadie <bastien.abadie@gmail.com>
2014-03-04 23:16:38 +0100

    Add default templates for flickr sets.

--- generator.py
+++ generator.py
@@ -2,11 +2,22 @@ from pelican import generators
 import re
 import unicodedata
 import main
+import os
 
 class FlickrGenerator(generators.Generator):
   template_source = 'flickr_set.html'
 
+  def __init__(self, context, settings, path, theme, output_path, **kwargs):
+
+    # Add our templates dir to _templates_path
+    paths = settings.get('EXTRA_TEMPLATES_PATHS', [])
+    paths.append(os.path.join(os.path.dirname(__file__), 'templates'))
+    settings['EXTRA_TEMPLATES_PATHS'] = paths
+
+    super(FlickrGenerator, self).__init__(context, settings, path, theme, output_path, **kwargs)
+
   def generate_output(self, writer):
+
     for photoset in main.FLICKR_CACHE.sets:
       self.generate_photoset(writer, photoset)
 
